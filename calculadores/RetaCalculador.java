f6a5579725b17da2d5a7c277b5087fe55ce00973
arthur.ta@hotmail.com <ra00171754@D-094026.pucsp.br>
2018-08-23 18:34:35 -0300

    Criacao do circulo

---
+++ calculadores/RetaCalculador.java
@@ -0,0 +1,64 @@
+package calculadores;
+
+import java.util.ArrayList;
+import java.util.List;
+import primitivos.Ponto;
+import primitivos.Reta;
+
+public class RetaCalculador {
+	
+	public static List<Ponto> obterPontos(Reta reta){
+		double m    = reta.getCoeficienteAngular();
+		double b    = reta.getA().gety() - m * reta.getA().getx();
+		int x1      = (int) Math.floor(reta.getA().getx());
+		int x2      = (int) Math.floor(reta.getB().getx());
+		int y1      = (int) Math.floor(reta.getA().gety());
+		int y2      = (int) Math.floor(reta.getB().gety());
+		int deltaX  = (x1 >= x2 ) ? Math.abs(x1-x2) : Math.abs(x2-x1);
+		int deltaY	=  (y1 >= y2) ? Math.abs(y1-y2) : Math.abs(y2-y1);
+		List<Ponto> pontos = new ArrayList<>();
+
+		if(x2 < x1){
+		    int aux = x1;
+		    x1 = x2;
+		    x2 = aux;
+        }
+
+        if(y2 < y1){
+            int aux = y1;
+            y1 = y2;
+            y2 = aux;
+        }
+
+		if (deltaX == 0 ){
+			//TODO: Loop que só anda no y: Linha vertical
+			for(int y = y1; y <= y2; y++){
+				pontos.add(new Ponto(x1, y));
+			}
+		}
+		else if (deltaY == 0){
+			//TODO: Loop que só anda no x: Linha horizontal
+			for(int x = x1; x <= x2; x++){
+				pontos.add(new Ponto(x, y1));
+			}
+		}
+		else if (deltaX > deltaY){
+			//TODO: Loop que usa o x para achar o y
+			for(int x = x1; x <= x2; x++){
+				int y_atual = (int) Math.floor(m * x + b);
+				pontos.add(new Ponto(x, y_atual));
+			}
+		}
+		else{
+			//TODO: Loop que usa o y para achar o x
+			for(int y = y1; y <= y2; y++){
+				int x_atual = (int) Math.floor( (y - b)/ m );
+				pontos.add(new Ponto(x_atual, y));
+			}
+		}
+
+		return pontos;
+	}
+
+
+}
---
+++ primitivos/Reta.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package primitivos;
+
+/**
+ * @author ra00170556
+ *
+ */
+
+public class Reta {
+
+	Ponto a;
+	Ponto b;
+	Double coeficienteAngular;
+
+	public Reta(Ponto a, Ponto b) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.calcularCoeficienteAngular(a, b);
+	}
+
+	private void calcularCoeficienteAngular (Ponto a, Ponto b){
+		// TODO: Implementar : a.getx() - b.getx() != 0
+		this.coeficienteAngular = (a.gety()- b.gety())/(a.getx()- b.getx());
+	}
+
+	public Ponto getA() {
+		return a;
+	}
+
+	public void setA(Ponto a) {
+		this.a = a;
+		this.calcularCoeficienteAngular(this.a, this.b);
+	}
+
+	public Ponto getB() {
+		return b;
+	}
+
+	public void setB(Ponto b) {
+		this.b = b;
+		this.calcularCoeficienteAngular(this.a, this.b);
+	}
+
+	public Double getCoeficienteAngular() {
+		return coeficienteAngular;
+	}
+
+	public void setCoeficienteAngular(Double coeficienteAngular) {
+		this.coeficienteAngular = coeficienteAngular;
+	}	
+}
